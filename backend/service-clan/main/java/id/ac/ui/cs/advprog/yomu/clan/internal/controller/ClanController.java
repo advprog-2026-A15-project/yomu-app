5d5c8c9e87688f2981e5a519c851e01c3a8b2c6a
Christna Yosua Rotinsulu <yosuamic123@gmail.com>
2026-05-02 20:23:38 +0700

    [Refactoring] - implementasi sistem liga dengan Strategy Pattern untuk scoring

--- backend/service-clan/main/java/id/ac/ui/cs/advprog/yomu/clan/internal/controller/ClanController.java
+++
@@ -1,4 +0,0 @@
-package id.ac.ui.cs.advprog.yomu.clan.internal.controller;
-
-public class ClanController {
-}
--- backend/service-clan/main/java/id/ac/ui/cs/advprog/yomu/clan/internal/service/ClanServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package id.ac.ui.cs.advprog.yomu.clan.internal.service;
-
-public class ClanServiceImpl {
-}
